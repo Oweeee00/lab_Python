690fbebd81b8e9677a2730209968b50775b72abc
Oweeee00 <147934634+Oweeee00@users.noreply.github.com>
2023-10-14 18:39:21 +0300

    Add files via upload

---
+++ Лабораторная 1/Task3.py
@@ -0,0 +1,7 @@
+list_players = ["Маша", "Петя", "Саша", "Оля", "Кирилл", "Коля"]
+mid = len(list_players) // 2
+# TODO Разделите участников на две команды
+team_1 = list_players[:mid]
+team_2 = list_players[mid:]
+print(team_1)
+print(team_2)
